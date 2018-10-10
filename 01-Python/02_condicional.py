4bdb971fd99157d67a861f973fe9b0352be78650
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2018-10-10 08:56:30 -0500

    Condicionales

---
+++ 01-Python/02_condicional.py
@@ -0,0 +1,19 @@
+# Truty
+# Falsy
+
+if True:
+    print("It's true")
+else:
+    print("It's not true")
+
+if False:
+    print("It's true")
+else:
+    print("It's not true")
+
+name = 'Daniel'
+dictionary = []
+if dictionary:  # Truty
+    print('Yes')
+else:
+    print('No')
