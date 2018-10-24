4952d538bb184981e2e9cd1ab2c57d5db2a3be09
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2018-10-24 07:25:02 -0500

    Funciones lambda

---
+++ 01-Python/08_funciones.py
@@ -0,0 +1,11 @@
+#  lambda functions
+elevar_al_cuadrado = lambda numero: numero * numero
+
+elevar_al_cuadrado = lambda n: n * n
+
+print(elevar_al_cuadrado(4))
+
+add_two_numbers_v2 = lambda x, y: x + y
+
+print(add_two_numbers_v2(4, 4))
+
